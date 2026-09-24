1ab425e4fdd28345a2d222d32beebf5fa17afb3d
Ehsan Amn <amanianehsan15@gmail.com>
2026-09-24 20:02:13 +0330

    db: add editable case study fields

---
+++ prisma/migrations/20260924200000_project_case_study/migration.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "Project"
+  ADD COLUMN "clientName" TEXT,
+  ADD COLUMN "role" TEXT,
+  ADD COLUMN "challenge" TEXT,
+  ADD COLUMN "solution" TEXT,
+  ADD COLUMN "impact" TEXT,
+  ADD COLUMN "technologies" TEXT,
+  ADD COLUMN "galleryUrls" TEXT;
